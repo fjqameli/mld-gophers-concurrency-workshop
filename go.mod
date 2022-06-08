a805fc860ff36bca1b60b455481f0796644064ed
Francisco Quintero <francisco.quintero@mercadolibre.com.co>
2022-06-08 17:58:44 -0500

    ping pong table config

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fjqameli/mld-gophers-concurrency-workshop
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Ball struct {
+	hits int
+}
+
+type Player struct {
+	name  string
+	table chan *Ball
+}
+
+func main() {
+	table := make(chan *Ball)
+	p1 := Player{name: "ping", table: table}
+	p2 := Player{name: "pong", table: table}
+	go p1.Play()
+	go p2.Play()
+	p1.Serve()
+
+}
+
+func (p *Player) Serve() {
+	p.table <- new(Ball)
+	time.Sleep(1 * time.Second)
+}
+
+func (p *Player) Play() {
+	for {
+		ball := <-p.table
+		ball.hits++
+		fmt.Println(p.name, ball.hits)
+		time.Sleep(100 * time.Microsecond)
+		p.table <- ball
+	}
+}
