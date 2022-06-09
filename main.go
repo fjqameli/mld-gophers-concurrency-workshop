aeb268d33dfb0771c47100acffac3abb06eb0479
Francisco Quintero <francisco.quintero@mercadolibre.com.co>
2022-06-09 13:33:39 -0500

    closure

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 func main() {
 	//runtime.GOMAXPROCS(1)
 	for i := 0; i < 9; i++ {
-		go func() {
-			fmt.Println(i)
-		}()
+		go func(tmp int) {
+			fmt.Println(tmp)
+		}(i)
 	}
 	time.Sleep(1 * time.Second)
 }
