98a38d116d49485a18d3d3bc24ba0b3e9c886c0c
Francisco Quintero <francisco.quintero@mercadolibre.com.co>
2022-06-09 13:30:13 -0500

    closure

--- main.go
+++ main.go
@@ -5,36 +5,12 @@ import (
 	"time"
 )
 
-type Ball struct {
-	hits int
-}
-
-type Player struct {
-	name  string
-	table chan *Ball
-}
-
 func main() {
-	table := make(chan *Ball)
-	p1 := Player{name: "ping", table: table}
-	p2 := Player{name: "pong", table: table}
-	go p1.Play()
-	go p2.Play()
-	p1.Serve()
-
-}
-
-func (p *Player) Serve() {
-	p.table <- new(Ball)
-	time.Sleep(1 * time.Second)
-}
-
-func (p *Player) Play() {
-	for {
-		ball := <-p.table
-		ball.hits++
-		fmt.Println(p.name, ball.hits)
-		time.Sleep(100 * time.Millisecond)
-		p.table <- ball
+	//runtime.GOMAXPROCS(1)
+	for i := 0; i < 9; i++ {
+		go func() {
+			fmt.Println(i)
+		}()
 	}
+	time.Sleep(1 * time.Second)
 }
