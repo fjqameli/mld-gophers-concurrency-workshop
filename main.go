6661d6ec265889212af496c2d1a2afa1a05ea550
Francisco Quintero <francisco.quintero@mercadolibre.com.co>
2022-06-09 14:18:54 -0500

    reordering

--- main.go
+++ main.go
@@ -1,40 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"runtime"
 	"time"
 )
 
-type Ball struct {
-	hits int
+var _ = runtime.GOMAXPROCS(1)
+
+var a, b int
+
+func u1() {
+	a = 1
+	b = 2
+	println("corriendo u1")
 }
 
-type Player struct {
-	name  string
-	table chan *Ball
+func u2() {
+	a = 3
+	b = 4
+	println("corriendo u2")
+}
+
+func p() {
+	println(a)
+	println(b)
 }
 
 func main() {
-	table := make(chan *Ball)
-	p1 := Player{name: "ping", table: table}
-	p2 := Player{name: "pong", table: table}
-	go p1.Play()
-	go p2.Play()
-	p1.Serve()
-
-}
-
-func (p *Player) Serve() {
-	p.table <- new(Ball)
+	go u1()
+	go u2()
+	go p()
 	time.Sleep(1 * time.Second)
-}
-
-func (p *Player) Play() {
-	for {
-		ball := <-p.table
-		ball.hits++
-		fmt.Println(p.name, ball.hits)
-		time.Sleep(100 * time.Millisecond)
-		p.table <- ball
-	}
 }
